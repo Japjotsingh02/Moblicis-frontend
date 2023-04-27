1f97a1caa2fe07664c57cac9c995d3486016acf3
Japjotsingh02 <sjapjot2002@gmail.com>
2023-04-27 17:39:13 +0530

    frontend updated

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TableContainer from '@mui/material/TableContainer';
-import { Paper, TableBody, TableCell, TableHead, TableRow, styled, tableCellClasses } from '@mui/material';
-
-const BASE_URL="https://mobilicis-server.onrender.com/user";
+import { Box, CircularProgress, Paper, TableBody, TableCell, TableHead, TableRow, styled, tableCellClasses } from '@mui/material';
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -15,52 +13,77 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     },
 }));
 
-function Table({endPoint,reqBody}) {
-    const [DataArray,setDataArray]=useState([]);
-    const fetchData=()=>{
-        axios.post(`${BASE_URL}/${endPoint}`,reqBody)
-            .then((data)=>{
-                setDataArray(data);
-            })
-            .catch((err)=>{
-                alert(err.measssge || err);
-            })
-    };
+const fetchData=async(endPoint,reqBody)=>{
+  const response=await axios.post(`${process.env.REACT_APP_BASE_URL}/${endPoint}`,reqBody);
 
+  const data = await response.data;
+  console.log(data);
+  return data;
+};
+
+const Table=({endPoint,reqBody})=> {
+    const [DataArray,setDataArray]=useState(null);
+    const [IsLoading,setIsLoading]=useState(true);
+        
     useEffect(() => {
-        fetchData();
+      async function loadData(){
+        try{
+          // if(!DataArray){
+            const res=await fetchData(endPoint,reqBody);
+            setDataArray(res);
+            setIsLoading(false);
+          // }
+        }
+        catch(err){
+          console.error(err);
+        }
+      }
+      loadData();
     }, []);
-    console.log(DataArray);
+
     return (
         <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 700 }} aria-label="customized table">
-          <TableHead>
-            <TableRow>
-                {Object.keys(DataArray[0]).map((i)=>{
-                    console.log(i);
-                    return <StyledTableCell>i</StyledTableCell>;
-                })}
-              <StyledTableCell>Dessert (100g serving)</StyledTableCell>
-              <StyledTableCell align="right">Calories</StyledTableCell>
-              <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-              <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-              <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {/* {rows.map((row) => (
-              <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                  {row.name}
-                </StyledTableCell>
-                <StyledTableCell align="right">{row.calories}</StyledTableCell>
-                <StyledTableCell align="right">{row.fat}</StyledTableCell>
-                <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-                <StyledTableCell align="right">{row.protein}</StyledTableCell>
-              </StyledTableRow>
-            ))} */}
-          </TableBody>
-        </Table>
+          {IsLoading ?
+          <Box
+            width={"100%"}
+            m='1rem auto'
+            display='flex'
+            alignItems={"center"}
+            justifyContent={"center"}
+            height={'80vh'}
+          >
+            <CircularProgress/>
+          </Box>
+         :
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                  {/* {Object.keys(DataArray[0]).map((i)=>{
+                      console.log(i);
+                      return <StyledTableCell>i</StyledTableCell>;
+                  })} */}
+                <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+                <StyledTableCell align="right">Calories</StyledTableCell>
+                <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+                <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+                <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {/* {rows.map((row) => (
+                <StyledTableRow key={row.name}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.name}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.calories}</StyledTableCell>
+                  <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                  <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+                  <StyledTableCell align="right">{row.protein}</StyledTableCell>
+                </StyledTableRow>
+              ))} */}
+            </TableBody>
+          </Table>
+        }
       </TableContainer>
     );
 }
